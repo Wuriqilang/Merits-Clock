1965eaba20c82ec9237e97e082288b2f3852dd94
Wuriqilang <543677991@qq.com>
2019-05-28 18:53:37 +0800

    111

--- pages/basics/home/home.js
+++ pages/basics/home/home.js
@@ -24,30 +24,25 @@ Component({
       badge: 22,
       name: '通知'
     },],
-    elements: [
-      { title: '新增资产', name: 'add', color: 'cyan', icon: 'newsfill' },
-      { title: '资产列表', name: 'list', color: 'blue', icon: 'searchlist' },
-      { title: '资产报表', name: 'form', color: 'red', icon: 'formfill' },
-      { title: '资产调整', name: 'change', color: 'olive', icon: 'album' },
-    ], elements2: [
-      { title: '用户管理', name: 'people', color: 'green', icon: 'people' },
-      { title: '发布公告', name: 'notice', color: 'orange', icon: 'notice' },
-    ],
-    cardCur: 0,
-    swiperList: [{
-      id: 0,
-      type: 'image',
-      url: '/images/swiper/1.jpg'
-    }, {
-      id: 1,
-      type: 'image',
-        url: '/images/swiper/2.jpg',
-    }, {
-      id: 2,
-      type: 'image',
-				url: '/images/swiper/4.jpg'
-    }],
+		year: 0,
+		month: 0,
+		date: ['日', '一', '二', '三', '四', '五', '六'],
+		dateArr: [],
+		isToday: 0,
+		isTodayWeek: false,
+		todayIndex: 0
   },
+	created(){
+		let now = new Date();
+		let year = now.getFullYear();
+		let month = now.getMonth() + 1;
+		this.dateInit();
+		this.setData({
+			year: year,
+			month: month,
+			isToday: '' + year + month + now.getDate()
+		})
+	},
   methods: {
     onLoad() {
       let that = this;
@@ -62,28 +57,90 @@ Component({
         }
       })
     },
-		// cardSwiper
-		cardSwiper(e) {
-			this.setData({
-				cardCur: e.detail.current
-			})
-		},
-    showModal(e) {
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
-    },
-    hideModal(e) {
-      this.setData({
-        modalName: null
-      })
-    },
     onShareAppMessage() {
       return {
         title: 'ColorUI-高颜值的小程序UI组件库',
         imageUrl: 'https://image.weilanwl.com/color2.0/share2215.jpg',
         path: '/pages/basics/home/home'
       }
-    },
+		}, 
+		dateInit: function (setYear, setMonth) {
+			//全部时间的月份都是按0~11基准，显示月份才+1
+			let dateArr = [];                       //需要遍历的日历数组数据
+			let arrLen = 0;                         //dateArr的数组长度
+			let now = setYear ? new Date() : new Date();
+			let year = setYear || now.getFullYear();
+			let nextYear = 0;
+			let month = setMonth || now.getMonth();                 //没有+1方便后面计算当月总天数
+			let nextMonth = (month + 1) > 11 ? 1 : (month + 1);
+			let startWeek = new Date().getDay();                          //目标月1号对应的星期
+			let dayNums = new Date().getDate();               //获取目标月有多少天
+			let obj = {};
+			let num = 0;
+			if (month + 1 > 11) {
+				nextYear = year + 1;
+				dayNums = new Date().getDate();
+			}
+			arrLen = startWeek + dayNums;
+			for (let i = 0; i < arrLen; i++) {
+				if (i >= startWeek) {
+					num = i - startWeek + 1;
+					obj = {
+						isToday: '' + year + (month + 1) + num,
+						dateNum: num,
+						weight: 5
+					}
+				} else {
+					obj = {};
+				}
+				dateArr[i] = obj;
+			}
+			this.setData({
+				dateArr: dateArr
+			})
+			let nowDate = new Date();
+			let nowYear = nowDate.getFullYear();
+			let nowMonth = nowDate.getMonth() + 1;
+			let nowWeek = nowDate.getDay();
+			let getYear = setYear || nowYear;
+			let getMonth = setMonth >= 0 ? (setMonth + 1) : nowMonth;
+			if (nowYear == getYear && nowMonth == getMonth) {
+				this.setData({
+					isTodayWeek: true,
+					todayIndex: nowWeek
+				})
+			} else {
+				this.setData({
+					isTodayWeek: false,
+					todayIndex: -1
+				})
+			}
+		},
+		/**
+		 * 上月切换
+		 */
+		lastMonth: function () {
+			//全部时间的月份都是按0~11基准，显示月份才+1
+			let year = this.data.month - 2 < 0 ? this.data.year - 1 : this.data.year;
+			let month = this.data.month - 2 < 0 ? 11 : this.data.month - 2;
+			this.setData({
+				year: year,
+				month: (month + 1)
+			})
+			this.dateInit(year, month);
+		},
+		/**
+		 * 下月切换
+		 */
+		nextMonth: function () {
+			//全部时间的月份都是按0~11基准，显示月份才+1
+			let year = this.data.month > 11 ? this.data.year + 1 : this.data.year;
+			let month = this.data.month > 11 ? 0 : this.data.month;
+			this.setData({
+				year: year,
+				month: (month + 1)
+			})
+			this.dateInit(year, month);
+		}
   },
 })
